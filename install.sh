f462dbef723039d9f636fb77e61160c350fffa50
Chris Paxton <paxton.chris@live.com>
2024-10-16 18:29:10 +0400

    make sure install script dies if it dies

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "Install basic dependencies..."
 sudo apt update
 sudo apt install -y build-essential git git-lfs vim-gtk3 byobu cmake htop feh python-is-python3 libpython3.10-dev curl net-tools
